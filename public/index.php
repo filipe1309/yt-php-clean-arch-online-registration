765663a2d66ed966d38e3c8d62131d787b5ad201
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-06 13:43:20 -0300

    chore: add registration example

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Domain\Entity\Registration;
+use App\Domain\ValueObject\Cpf;
+use App\Domain\ValueObject\Email;
+
 require_once '../vendor/autoload.php';
 
-phpinfo();
+$registration =  new Registration;
+$registration->setName('John')
+    ->setBirthDate(new DateTimeImmutable('2010-01-02'))
+    ->setEmail(new Email('john.doe@test.com'))
+    ->setRegistrationAt(new DateTimeImmutable())
+    ->setRegistrationNumber(new Cpf('370.100.370-00'));
+
+echo '<pre>';
+var_dump($registration);
