fc5ef9d4c5c6da4b7f3efae177fbdf5e9877151c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-06 13:42:47 -0300

    feat: add value objects

---
+++ src/Domain/ValueObject/Cpf.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Domain\ValueObject;
+
+use DomainException;
+
+final class Cpf
+{
+    private string $cpf;
+
+    public function __construct(string $cpf)
+    {
+        if (!$this->validate($cpf)) {
+            throw new DomainException('Invalid cpf');
+        }
+
+        $this->cpf = $cpf;
+    }
+
+    private function validate($cpf)
+    {
+
+        // Extrai somente os números
+        $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+
+        // Not 11 digits or all repeated. Ex: 111.111.111-11
+        if ((strlen($cpf) != 11) || preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        // Module 11 to validate CPF
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function __toString(): string
+    {
+        return $this->cpf;
+    }
+}
