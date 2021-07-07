2ca410693d89d75d59d46bfb34291c4c979fdd8a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-07 13:33:15 -0300

    refactor: input & output boudaries

--- src/Application/UseCase/ExportRegistration/InputBoundary.php
+++ src/Application/UseCase/ExportRegistration/InputBoundary.php
@@ -5,12 +5,24 @@ namespace App\Application\UseCase\ExportRegistration;
 final class InputBoundary
 {
     public function __construct(
-        private string $registrationNumber
+        private string $registrationNumber,
+        private string $pdfFileName,
+        private string $path,
     ) {
     }
 
     public function getRegistrationNumber(): string
     {
         return $this->registrationNumber;
+    }
+
+    public function getPdfFileName(): string
+    {
+        return $this->pdfFileName;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
     }
 }
--- src/Application/UseCase/ExportRegistration/OutputBoundary.php
+++ src/Application/UseCase/ExportRegistration/OutputBoundary.php
@@ -4,43 +4,13 @@ namespace App\Application\UseCase\ExportRegistration;
 
 final class OutputBoundary
 {
-    private string $name;
-    private string $email;
-    private string $birthDate;
-    private string $registrationNumber;
-    private string $registrationAt;
-
-    public function __construct(array $values)
-    {
-        $this->name = $values['name'] ?? '';
-        $this->email = $values['email'] ?? '';
-        $this->birthDate = $values['birthDate'] ?? '';
-        $this->registrationNumber = $values['registrationNumber'] ?? '';
-        $this->registrationAt = $values['registrationAt'] ?? '';
+    public function __construct(
+        private string $fullFileName
+    ) {
     }
 
-    public function getName(): string
+    public function getFullFileName()
     {
-        return $this->name;
-    }
-
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function getBirthDate(): string
-    {
-        return $this->birthDate;
-    }
-
-    public function getRegistrationNumber(): string
-    {
-        return $this->registrationNumber;
-    }
-
-    public function getRegistrationAt(): string
-    {
-        return $this->registrationAt;
+        return $this->fullFileName;
     }
 }
