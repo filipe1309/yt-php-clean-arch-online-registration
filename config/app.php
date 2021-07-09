895dcb11c746901a90075dc802cffcd60348cd9e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-07-08 22:41:41 -0300

    feat: add db app config

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'mysql',
+        'username' => 'root',
+        'password' => 'root',
+        'port' => '3306',
+        'dbname' => 'development',
+        'charset' => 'utf8',
+    ]
+];
